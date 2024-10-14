df51de246a94e4d9214a7c611c1ec1fc63452c08
Sofia Nuñez <sofia.nunez@alumnos.ucn.cl>
2024-10-14 00:16:06 -0300

    logica

---
+++ Revista.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "Revista.h"
+
+using namespace std;
+
+Revista::Revista(string nombre, string isbn, string autor, bool prestado, int numeroEdicion, string mesPublicacion) : MaterialBibliografico(nombre, isbn, autor, prestado), numeroEdicion(numeroEdicion), mesPublicacion(mesPublicacion){
+    this -> nombre = nombre;
+    this -> isbn = isbn;
+    this -> autor = autor;
+    this -> prestado = false;
+    this -> numeroEdicion = numeroEdicion;
+    this -> mesPublicacion = mesPublicacion;
+}
+
+void Revista:: mostrarInformacion() {
+    cout<<"Nombre: "<<nombre<<endl;
+    cout<<"Isbn: "<<isbn<<endl;
+    cout<<"Autor: "<<autor<<endl;
+    cout<<"Prestado?: "<<prestado<<endl;
+    cout<<"Numero de Edición: "<<numeroEdicion<<endl;
+    cout<<"Mes de Publicación: "<<mesPublicacion<<endl;
+}
+
+int Revista:: getNumeroEdicion() {
+    return this -> numeroEdicion;
+}
+
+void Revista:: setNumeroEdicion(int numeroEdicion) {
+    this -> numeroEdicion = numeroEdicion;
+}
+
+string Revista:: getMesPublicacion() {
+    return this -> mesPublicacion;
+}
+
+void Revista:: setMesPublicacion(string mesPublicacion) {
+    this -> mesPublicacion = mesPublicacion;
+}
+
+Revista :: ~Revista(){
+    cout<<"se destruye esta revista"<<endl;
+}
---
+++ Usuario.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#pragma once
+#include "MaterialBibliografico.h"
+
+class Usuario {
+    private:
+        string nombre;
+        int id;
+        MaterialBibliografico* materialesPrestados[5];
+    public:
+        Usuario(string nombre, int id, MaterialBibliografico* materialesPrestados[5]);
+        ~Usuario();
+        void prestarMaterial();
+        void devolverMaterial();
+        void mostrarMaterialesPrestados();
+};
